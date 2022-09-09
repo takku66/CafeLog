566be2f7a9d9af83072640e8a4b55d3d7ddc1e30
takku66 <tattaka15@gmail.com>
2022-09-09 23:32:02 +0900

    MyBatisの記述方針を変更

--- src/main/java/com/cafelog/mapper/CafeLogUserMapper.java
+++ src/main/java/com/cafelog/mapper/CafeLogUserMapper.java
@@ -1,11 +1,18 @@
 package com.cafelog.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.cafelog.entity.CafeLogUser;
 
 @Mapper
 public interface CafeLogUserMapper {
     
+    
+    @Select("""
+    select user_id, name, email
+    from users
+    where email = #{email}
+    """)
     public CafeLogUser findByEmail(String email);
 }
--- src/main/java/com/cafelog/mapper/FavoritesMapper.java
+++ src/main/java/com/cafelog/mapper/FavoritesMapper.java
@@ -3,11 +3,19 @@ package com.cafelog.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.cafelog.entity.Cafe;
 
 @Mapper
 public interface FavoritesMapper {
 
+    @Select("""
+        select cafe.cafe_id, cafe.name, cafe.latitude, cafe.longitude
+        from favorites as fav
+        left outer join cafe
+            on fav.user_id = #{user_id} and
+                fav.cafe_id = cafe.cafe_id
+            """)
     public List<Cafe> findByUserId(int userId);
 }
