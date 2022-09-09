bbc39a2a4c2ef7f0fa77fda78742c63d3c30eb93
takku66 <tattaka15@gmail.com>
2022-09-10 01:41:53 +0900

    コミット漏れ

---
+++ src/main/resources/static/js/shopdata.js
@@ -0,0 +1,27 @@
+let shopdata;
+
+function fetchShopData(){
+    const csrfHeader = document.querySelector('meta[name="_csrf_header"]')?.content || "";
+    const token = document.querySelector('meta[name="_csrf"]')?.content || "";
+    const url = (CONTEXT_ROOT == "/" ? "" : CONTEXT_ROOT) + "/allcafe";
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "charset": "UTF-8",
+            "Content-Type": "application/json",
+            [csrfHeader]: token,
+        },
+    }).then((res) => {
+        if (!res.ok) {
+            throw new Error(`${res.status}${res.statusText}`);
+        }
+        console.log(res);
+        return res.text();
+    }).then((data) => {
+        console.log(data);
+        shopdata = JSON.parse(data);
+    }).catch((reason) => {
+        console.log(reason);
+    });
+}
+fetchShopData();
